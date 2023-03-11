d8dbacef9f9a807f86fa742addf6ffb182d473fd
annabelle-ep <annabelle.purnomo@gmail.com>
2023-03-11 01:33:23 -0800

    Added in Roxygen documentation + added in running code-- TODO: fix issue with bars being same size, add test_that testings and exceptions

--- print_histogram.r
+++ print_histogram.r
@@ -1,16 +1,34 @@
-# author: Annabelle Purnomo
-# date: 2023-03-10
+#' Creates a histogram plot visualisation from our csv file
+#'
+#' @param data Tidy data containing a list of genres and acoustic features 
+#' @param feature Specified acoustic feature of which we want a visualisation
+#'
+#' @return a histogram plot 
+#' @export
+#'
+#' @examples
 
-"This script creates a histogram plot visualisation from our csv file. 
-This script takes tidy data and a data file path.
+library(tidyverse)
+library(testthat)
 
-Usage: create_histogram.R ./data/spotify_songs.csv
-"
+# potentially temporarily reading in data to test. create a new .r file for this?
+x <- getURL("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv")
+song_data <- read.csv(text = x)
+tidy_song_data <- song_data |>
+  select(playlist_genre, danceability:tempo)
 
-create_histogram(data,feature,bin_num) {
-    ggplot(data, aes(x = feature)) + 
-    geom_histogram(bins = bin_num) + 
+# actual code
+create_histogram <- function(df,feature) {
+  histogram <- ggplot(df, aes(x = {{feature}})) + 
+    geom_histogram(bins = 20, stat="count") + 
     facet_grid(rows = "playlist_genre")
+  histogram
 }
- 
-create_histogram(training_song_data, danceability,20)
+
+create_histogram(tidy_song_data, "danceability")
+create_histogram(tidy_song_data, "tempo")
+
+
+create_histogram(tidy_song_data, training_song_data)
+
+
